6d038799f824db06118a3b145216998c24b4eef1
Nico <nico.riedlinger@gmx.de>
2021-11-13 21:05:47 +0100

    Made db multithread safe

--- classes/Database.py
+++ classes/Database.py
@@ -6,9 +6,10 @@ from datetime import datetime
 class Database:
     def __init__(self):
         self.name = 'mpgbot.db'
-        self.connection = sqlite3.connect(self.name)
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS Orders (\
+
+        connection = sqlite3.connect(self.name)
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS Orders (\
             id INTEGER,\
             type VARCHAR(16),\
             side VARCHAR(5),\
@@ -18,29 +19,36 @@ class Database:
             timestamp INTEGER,\
             PRIMARY KEY (id)\
         )")
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS Profits (\
+        cursor.execute("CREATE TABLE IF NOT EXISTS Profits (\
             id INTEGER,\
             value REAL,\
             timestamp TIMESTAMP,\
             PRIMARY KEY (id)\
         )")
-        self.connection.commit()
+        connection.commit()
+        cursor.close()
+        connection.close()
 
     def __str__(self):
         return "{name:" + self.name + "}"
 
-    def close_connection(self):
-        self.cursor.close()
-        self.connection.close()
-        self.connection = None
-
     def save_order(self, order):
-        self.cursor.execute("INSERT INTO Orders VALUES (%s, '%s', '%s', %s, %s, '%s', %s)" % (order['id'], order['type'], order['side'], order['price'], order['amount'], order['status'], order['timestamp']))
-        self.connection.commit()
+        connection = sqlite3.connect(self.name)
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO Orders VALUES (%s, '%s', '%s', %s, %s, '%s', %s)" % (
+            order['id'], order['type'], order['side'], order['price'], order['amount'], order['status'], order['timestamp']))
+        connection.commit()
+        cursor.close()
+        connection.close()
 
     def save_profit(self, profit):
-        self.cursor.execute("INSERT INTO Profits VALUES(value, timestamp) (?, ?)" % (profit, datetime.now()))
-        self.connection.commit()
+        connection = sqlite3.connect(self.name)
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO Profits VALUES(value, timestamp) (?, ?)" % (
+            profit, datetime.now()))
+        connection.commit()
+        cursor.close()
+        connection.close()
 
     def update_order_status(self, order):
         # switch status names
@@ -51,31 +59,53 @@ class Database:
         # only update when value has changed
         order_db = self.read_order_by_id(order['id'])
         if not order_db == None and not status == order_db[5]:
-            self.cursor.execute("UPDATE Orders SET status='%s' WHERE id=%s" % (status, order['id']))
-            self.connection.commit()
+            connection = sqlite3.connect(self.name)
+            cursor = connection.cursor()
+            cursor.execute(
+                "UPDATE Orders SET status='%s' WHERE id=%s" % (status, order['id']))
+            connection.commit()
+            cursor.close()
+            connection.close()
 
             # info message
-            log.info("Order id " + order['id'] + " changed status to: " + status)
+            log.info("Order id " + order['id'] +
+                     " changed status to: " + status)
 
     def read_orders(self):
-        self.cursor.execute(
+        connection = sqlite3.connect(self.name)
+        cursor = connection.cursor()
+        cursor.execute(
             "SELECT * FROM Orders ORDER BY timestamp DESC LIMIT 300")
-        orders = self.cursor.fetchall()
+        orders = cursor.fetchall()
+        cursor.close()
+        connection.close()
 
         return orders
 
     def read_order_by_id(self, id):
-        self.cursor.execute("SELECT * FROM Orders WHERE id=%s" % id)
-        return self.cursor.fetchone()
+        connection = sqlite3.connect(self.name)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM Orders WHERE id=%s" % id)
+        order = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return order
 
     def clear_orders(self, ids=[]):
         if len(ids) <= 0:
-            self.cursor.execute("DELETE FROM Orders")
+            connection = sqlite3.connect(self.name)
+            cursor = connection.cursor()
+            cursor.execute("DELETE FROM Orders")
+            cursor.close()
+            connection.close()
         else:
             for id in ids:
                 self.clear_order(id)
 
-
     def clear_order(self, id):
-        self.cursor.execute("DELETE FROM Orders WHERE id=%s" % id)
-        self.connection.commit()
+        connection = sqlite3.connect(self.name)
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM Orders WHERE id=%s" % id)
+        connection.commit()
+        cursor.close()
+        connection.close()
